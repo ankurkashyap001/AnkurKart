ded608c4f65e2fdd48a6af3297b64bf3af77fa93
ankurkashyap001 <antalankur2@gmail.com>
2026-08-14 17:15:25 +0530

    app configration

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ankurkart.app',
+  appName: 'AnkurKart',
+  webDir: 'dist/ankur-kart/browser'
+};
+
+export default config;
